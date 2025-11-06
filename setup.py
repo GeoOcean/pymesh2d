58f97e837b63f1ee1c6e82cdde28cbd8a2ecdd73
FaugereE <faugeree@unican.es>
2025-11-06 13:02:22 +0100

    [EJP] change config in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # --------------------------- ONLY FOR DEVELOPMENT MODE ---------------------------
-# This setup.py file is used to configure the setup for the BlueMath_tk project.
+# This setup.py file is used to configure the setup for the pymesh2d project.
 # It specifies metadata about the project and its dependencies.
 # This file is ONLY useful for development mode, where you can install
 # the project in an editable state using the command `pip install -e .`.
@@ -10,20 +10,18 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="mesh2d_python",
+    name="pymesh2d",
     author="GeoOcean Group",
-    author_email="bluemath@unican.es",
-    #long_description=open("README.md").read(),
-    #long_description_content_type="text/markdown",
-    #url="https://github.com/GeoOcean/BlueMath_tk",
+    author_email="faugere@unican.es",
+    # long_description=open("README.md").read(),
+    # long_description_content_type="text/markdown",
+    # url="https://github.com/GeoOcean/pymesh2d",
     packages=find_packages(),  # Automatically find packages in the directory
     include_package_data=True,
     install_requires=[
         "numpy",
-        "sys",
-        "matplotlib",
-        "netcdf4",
+        "deloney",
     ],
-    classifiers=["Programming Language :: Python :: 3.11"],
-    python_requires=">=3.11",  # Specify the Python version required
+    classifiers=["Programming Language :: Python :: 3.3.7"],
+    python_requires=">=3.7",  # Specify the Python version required
 )
